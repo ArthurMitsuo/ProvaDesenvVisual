36e7417a72368164c65463e86a61479afa61bc74
Arthur Mitsuo <nkc_arthur@hotmail.com>
2023-10-09 18:57:25 -0300

    AppDataContext e outras classes(ainda vazias) criados

---
+++ API/Data/AppDataContext.cs
@@ -0,0 +1,21 @@
+﻿using API;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+public class AppDataContext : DbContext
+{
+     public AppDataContext(DbContextOptions<AppDataContext> options) : base(options)
+    {
+        
+    }
+
+    //Classes que vão se tornar tabelas no banco de dados
+    //public DbSet<Cargo> Cargos {get; set;}
+    //public DbSet<TarefaAtividade> TarefasAtividade {get; set;}
+
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
